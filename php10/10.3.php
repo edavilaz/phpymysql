0d8c0bc34a34ad69cb7ac5fc6aaf4996f5b0153f
edavilaz <edavilaz@gmail.com>
2020-10-04 14:12:13 -0500

    php10

---
+++ php10/10.3.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>Problema</title>
+    </head>
+    <body>
+        <form action="10.3a.php" method="post">
+            Ingrese dirección de sitio web (ej www.google.com):
+            <input type="text" name="direccion" size="30"><br>
+            <input type="submit" value="Redireccionar">
+        </form>
+    </body>
+</html> 
